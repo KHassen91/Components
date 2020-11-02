310166e98a5535724d734f8d326018639149b659
KHassen91 <hassen.knani.91@gmail.com>
2020-11-02 02:56:46 +0100

    v1

---
+++ src/Component/Profile/Adress.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Adress = () => {
+    return (
+        <p>Tunis Road Km9.5 Sakkiet Ezzit Sfax Tunisia</p>
+    );
+}
+
+export default Adress;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hassen from "./Hassen.jpg"
+const Photo = () => {
+    return (
+        <div>
+            <img style={{width:190 , height: 235 }}src={Hassen} alt='myImage' />
+        </div>
+    );
+}
+
+export default Photo ;
